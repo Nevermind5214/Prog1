8a83a763ae13e3ece81752325d1234dca62b0004
David Mikaczo <nevermind5214@gmail.com>
2021-03-16 21:01:30 +0100

    Saját mandelbrot undi fltk-val

--- week02-06/GUI/sajatmandel.cpp
+++ week02-06/GUI/sajatmandel.cpp
@@ -1,38 +1,46 @@
 /*
 g++ sajatmandel.cpp Graph.cpp Window.cpp GUI.cpp Simple_window.cpp -o sajatmandel `fltk-config --ldflags --use-images`
 */
+#include "Simple_window.h"
+#include "Graph.h"
+#include <complex>
 
-#include "Simple_window.h" // get access to our window library
-#include "Graph.h" // get access to our graphics library facilities
-#include <FL/Fl_Image.H>
+#define WIDTH 600
+#define HEIGHT 600
+
+int value (int x, int y)  {
+	complex<float> point((float)x/WIDTH-1.5, (float)y/HEIGHT-0.5);
+	complex<float> z(0, 0);
+	int iter = 0;
+	while (abs (z) < 2 && iter <= 20) {
+		z = z * z + point;
+		++iter;
+	}
+	if (iter < 20) return 1;
+	else return 0;
+}
 
 int main()
 try
 {
+	using namespace Graph_lib;
 
-	using namespace Graph_lib; // our graphics facilities are in Graph_lib
+	Simple_window win {Point{0, 0}, WIDTH, HEIGHT, "Mandelbrot"};
 
-	int xmax = 600; // ablak felbontása x
-	int ymax = 400; // ablak felbontása y
-	int x_orig = xmax/2; // ablak origója x
-	int y_orig = ymax/2; // ablak origója y
+	Vector_ref<Rectangle> rectangles;
 
+	for (int i = 0; i < WIDTH; ++i)
+	{
+		for (int j = 0; j < HEIGHT; ++j)
+		{
+			if(value(i,j)){
+				rectangles.push_back(new Rectangle{Point{i, j}, 1, 1});
+				win.attach(rectangles[rectangles.size()-1]);
+			}
+		}
+	}
 
-
-	Point origo {x_orig, y_orig}; // ablak origó koordinátái point típusban
-
-	Point tl {100,100}; // to become top left corner of window
-
-	Simple_window win {tl,xmax,ymax,"My window"}; // make a simple window
-
-
-	char buffer[128];
-	
-    fl_draw_image_mono(buffer+drawn*W,x()+3,yy,W,1,1,W);
-	fl_draw_image_mono(const uchar*, 0, 0, 600, 600, int D = 1, int LD = 0)
-
-	win.wait_for_button(); // give control to the display engine
-
+	win.wait_for_button();
 }
 
 catch (exception& e) {
